544066667824d4c22be67cbd00c3f4b2dc4b8292
Kestutis Arlauskas <kestutisa@gmail.com>
2020-07-05 18:07:24 +0300

    resolve ip escape $

--- install.sh
+++ install.sh
@@ -178,13 +178,13 @@ cat > $init_file <<EOF
 
 rm -rf .env
 
-export eth_interface=$(ip token | grep -v lo | grep -v br- | grep -v veth | grep -v docker | grep -v wlan | grep -v wlp | awk '{print $4}')
-export host_ip=$(ifconfig $eth_interface | grep inet | grep -v inet6 | awk '{print $2}')
+export eth_interface=\$(ip token | grep -v lo | grep -v br- | grep -v veth | grep -v docker | grep -v wlan | grep -v wlp | awk '{print \$4}')
+export host_ip=\$(ifconfig \$eth_interface | grep inet | grep -v inet6 | awk '{print \$2}')
 
-echo 'HOST_IP='$host_ip >> .env
+echo 'HOST_IP='\$host_ip >> .env
 echo '' >> .env
 
-echo 'Host ip' $host_ip 'added to .env file.'
+echo 'Host ip' \$host_ip 'added to .env file.'
 
 cat .env.dist >> .env
 echo '.env.dist content added to .env file.'
